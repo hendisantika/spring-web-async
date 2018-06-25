abffc99ed876b7a572b2705e6e9fa54f52e28410
Hendi Santika <hendisantika@gmail.com>
2018-06-26 06:57:22 +0700

    #4 - Add API Module

---
+++ api/src/main/java/com.hendisantika/entity/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+    private String name;
+    private String email;
+
+    Person() {
+
+    }
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
