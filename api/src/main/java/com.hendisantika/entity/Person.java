07ce1f27c78dab9ea66b8823d85f44fa77897edb
Hendi Santika <hendisantika@gmail.com>
2018-06-27 06:03:00 +0700

    #5 - Add Person Entity

--- api/src/main/java/com.hendisantika/entity/Person.java
+++ api/src/main/java/com.hendisantika/entity/Person.java
@@ -1,11 +1,23 @@
-public class Person {
+package com.hendisantika.entity;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-web-async
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/06/18
+ * Time: 06.01
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
     private String name;
     private String email;
 
     Person() {
 
     }
+
     public Person(String name, String email) {
         this.name = name;
         this.email = email;
