4f30f8c71bff37b16f90739e0286a352f9f3ab06
Hendi Santika <hendisantika@gmail.com>
2018-06-27 06:06:26 +0700

    #6 - Add Main App

---
+++ api/src/main/java/com.hendisantika/ApiApplication.java
@@ -0,0 +1,22 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-web-async
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/06/18
+ * Time: 06.06
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
